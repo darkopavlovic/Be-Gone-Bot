4fa61d4e936497bad24719c063b84ef9b6183bb9
Darko Pavlovic <44081311+darkopavlovic@users.noreply.github.com>
2020-05-04 16:33:12 -0600

    added TMI endpoint and react boilerplate

--- routes/channel.js
+++ routes/channel.js
@@ -6,11 +6,12 @@ const axios = require("axios");
 // Assign Twitch API variables
 const BASE_STREAM_URL = "https://api.twitch.tv/helix/streams";
 const BASE_OAUTH_URL = "https://id.twitch.tv/oauth2/token";
+const BASE_CHAT_URL = "https://tmi.twitch.tv/group/user";
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 // Get stream data from Twitch
-router.get("/:username", async (req, res) => {
+router.get("/stream/:username", async (req, res) => {
   try {
     await axios
       .post(`${BASE_OAUTH_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`)
@@ -20,6 +21,17 @@ router.get("/:username", async (req, res) => {
       .then((response) => {
         res.send(response.data);
       });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Get chat data from TMI
+router.get("/chat/:username", async (req, res) => {
+  try {
+    await axios.get(`${BASE_CHAT_URL}/${req.params.username}`).then((response) => {
+      res.send(response.data);
+    });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
