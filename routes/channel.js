c822c6e7df17e4f80895de7d3b0f94e8b95bd5e7
Darko Pavlovic <44081311+darkopavlovic@users.noreply.github.com>
2023-07-02 14:49:20 -0600

    Refactored channel endpoints

--- routes/channel.js
+++ routes/channel.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const axios = require("axios");
 
 // Assign Twitch API variables
-const BASE_STREAM_URL = "https://api.twitch.tv/helix/streams";
 const BASE_OAUTH_URL = "https://id.twitch.tv/oauth2/token";
+const BASE_STREAM_URL = "https://api.twitch.tv/helix/streams";
 const BASE_CHAT_URL = "https://tmi.twitch.tv/group/user";
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -12,14 +12,9 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 // Get stream data from Twitch
 router.get("/stream/:username", async (req, res) => {
   try {
-    await axios
-      .post(`${BASE_OAUTH_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`)
-      .then((response) => {
-        return axios.get(`${BASE_STREAM_URL}?user_login=${req.params.username}`, { headers: { "Client-ID": CLIENT_ID, "Authorization": `Bearer ${response.data.access_token}` } });
-      })
-      .then((response) => {
-        res.send(response.data.data[0]);
-      });
+    const credentials = await axios.post(`${BASE_OAUTH_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`);
+    const response = await axios.get(`${BASE_STREAM_URL}?user_login=${req.params.username}`, { headers: { "Client-ID": CLIENT_ID, "Authorization": `Bearer ${credentials.data.access_token}` } });
+    res.send(response.data.data[0]);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -28,9 +23,8 @@ router.get("/stream/:username", async (req, res) => {
 // Get chat data from TMI
 router.get("/chat/:username", async (req, res) => {
   try {
-    await axios.get(`${BASE_CHAT_URL}/${req.params.username}`).then((response) => {
-      res.send(response.data);
-    });
+    const response = await axios.get(`${BASE_CHAT_URL}/${req.params.username}`);
+    res.send(response.data);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
