08ce046a60541bcd6047f67bb725f3765130ed18
Darko Pavlovic <44081311+darkopavlovic@users.noreply.github.com>
2023-07-03 02:02:31 -0600

    Adding client path

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+// Prod environment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Health check route
 app.get("/health", (req, res) => {
   res.send("Hello from Server 👋");
